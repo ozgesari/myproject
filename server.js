bbb048dae21e8e3e077acb1e8478df660158f310
ozgesa <ozgesarri@gmail.com>
2019-11-26 10:26:24 +0000

    a

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const cors = require("cors");
 
 const app = express();
+
+
+const issue2options = {
+    origin: '*',
+    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+    credentials: true,
+    preflightContinue:false,
+    optionsSuccessStatus: 204,
+    maxAge: 3600
+  };
+app.options('*', cors());
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/todoapp'));
 
+app.post('/login', cors(issue2options), function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname,'/dist/todoapp/index.html'));
